1b80792d84e2bf163e461b2ee0ac11d4004eaab3
SirLynix <lynix680@gmail.com>
2025-04-09 21:29:07 +0200

    Network: Increase required client version

--- src/CommonLib/Protocol/Packets.cpp
+++ src/CommonLib/Protocol/Packets.cpp
@@ -96,18 +96,14 @@ namespace tsom
 						serializer &= shipInputs.moveBackward;
 						serializer &= shipInputs.moveLeft;
 						serializer &= shipInputs.moveRight;
+						serializer &= shipInputs.moveUp;
+						serializer &= shipInputs.moveDown;
 						serializer &= shipInputs.rollLeft;
 						serializer &= shipInputs.rollRight;
+						serializer &= shipInputs.stabilize;
 
 						serializer &= shipInputs.pitch;
 						serializer &= shipInputs.yaw;
-
-						if (serializer.GetProtocolVersion() >= BuildVersion(0, 6, 1))
-						{
-							serializer &= shipInputs.moveUp;
-							serializer &= shipInputs.moveDown;
-							serializer &= shipInputs.stabilize;
-						}
 					}
 				});
 			}
